ee2aa54662b56281bc94068e6dae741569c8f592
BORB <qzsec299@naver.com>
2026-03-26 16:56:23 +0900

    feat: 과제 1 완료

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from pybo.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('', index),
 ]
